8443c8b0467ff966991c60065ddf52ad3d14e024
Alberto Villena <alberto.villenaguerras@telefonica.com>
2018-10-31 09:29:36 +0100

    [FEATURE] Lesson 9

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,14 +1,13 @@
 "use strict";
 
+// LESSON 9 - This file is gonna be compiled by Babel
 // Here it goes React code. JSX.
 
 console.log("App.js is running!");
-
-// LESSON 9
 var template = React.createElement(
   "h1",
   null,
-  "Indecision App"
+  "Indecision App - Lesson 9"
 );
 var appRoot = document.getElementById("app");
 
