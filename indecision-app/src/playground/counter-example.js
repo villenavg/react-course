bde7d328dca6f571fe92e1d907a587d239974a05
Alberto Villena <alberto.villenaguerras@telefonica.com>
2018-12-18 14:45:58 +0100

    section4-lesson14

--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -36,29 +36,3 @@ class Counter extends React.Component {
 }
 
 ReactDOM.render(<Counter/>, document.getElementById('app'));
-
-/* let count = 0;
-const addOne = () => {
-    count++;
-    renderApp();
-};
-const minusOne = () => {
-    count--;
-    renderApp();
-};
-const reset = () => {
-    count = 0;
-    renderApp();
-};
-const renderApp = () => {
-    const template = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>Reset</button> 
-        </div>
-    );
-    ReactDOM.render(template, appRoot);
-};
-renderApp(); */
--- indecision-app/src/playground/visibility-toggle.js
+++ indecision-app/src/playground/visibility-toggle.js
@@ -1,20 +1,26 @@
-const appRoot = document.getElementById('app');
-let visible = false;
 
-const onShowToggle = () => {
-    visible = !visible;
-    render();
-};
-
-const render = () => {
-    const template = (
-        <div>
-            <h1>Visibility toggle!</h1>
-            <button onClick={onShowToggle}>{!visible ? "Show me the secret!" : "Hide the secret!"}</button>
-            {visible && <p>The secret is: BANANA!</p>}
-        </div>
-    );
-    ReactDOM.render(template, appRoot);
-};
-
-render();
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            visible: false,
+            secret: "Cuca"
+        }
+        this.handleVisibility = this.handleVisibility.bind(this);
+    }
+    handleVisibility(){
+        this.setState((prevState) => {
+           return {visible: !prevState.visible};
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h1>Visibility toggle!</h1>
+                <button onClick={this.handleVisibility}>{!this.state.visible ? "Show me the secret!" : "Hide the secret!"}</button>
+                {this.state.visible && <p>The secret is: {this.state.secret}</p>}
+            </div>
+        );
+    }
+}
+ReactDOM.render(<VisibilityToggle/>, document.getElementById('app'));
