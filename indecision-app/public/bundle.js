e67e84a97ccb9f34cb2a52051fc6c1c59f061991
Alberto Villena <alberto.villenaguerras@telefonica.com>
2018-12-21 14:31:32 +0100

    section6-lesson5

--- indecision-app/public/bundle.js
+++ indecision-app/public/bundle.js
@@ -71,18 +71,20 @@
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person_js__ = __webpack_require__(2);
 // import './utils.js';
-/* import { square, add } from './utils.js'
+/* import subtract, { add, square } from './utils.js'
 
 console.log('app.js is running!!!! ');
 console.log(square(4));
-console.log(add(1, 3)); */
+console.log(add(1, 3));
+console.log(subtract(1, 3)); */
 
 
-console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* isAdult */](17));
-console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* isAdult */](18));
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["c" /* isAdult */](17));
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["c" /* isAdult */](18));
 console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](18));
 console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](20));
 console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](21));
+console.log('isSenior: ', __WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* default */](64));
 
 /***/ }),
 /* 1 */,
@@ -91,11 +93,12 @@ console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](21));
 
 "use strict";
 const isAdult = (age) => age >= 18;
-/* harmony export (immutable) */ __webpack_exports__["b"] = isAdult;
+/* harmony export (immutable) */ __webpack_exports__["c"] = isAdult;
 
 const canDrink = (age) => age >= 21;
 /* harmony export (immutable) */ __webpack_exports__["a"] = canDrink;
 
+/* harmony default export */ __webpack_exports__["b"] = ((age) => age >= 65);
 
 /***/ })
 /******/ ]);
