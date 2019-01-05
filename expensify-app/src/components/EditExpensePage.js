f3494f077c16c02c9c7774fdefef423d07ad805d
Alberto Villena <alberto.villenaguerras@telefonica.com>
2019-01-05 19:10:28 +0100

    section9-lesson7

--- expensify-app/src/components/EditExpensePage.js
+++ expensify-app/src/components/EditExpensePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-    <div>
-    This is from my edit expense component
-    </div>
-);
+const EditExpensePage = (props) => {
+    console.log(props);
+    return (
+        <div>
+            Editing item with id: {props.match.params.id}
+        </div>
+    );
+}
 
 export default EditExpensePage;
