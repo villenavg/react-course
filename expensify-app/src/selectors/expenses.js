6894ef489631bfde9f7f448bafc88b756eec21c2
Alberto Villena <alberto.villenaguerras@telefonica.com>
2019-02-06 14:06:41 +0100

    section11-lesson13

--- expensify-app/src/selectors/expenses.js
+++ expensify-app/src/selectors/expenses.js
@@ -1,10 +1,12 @@
+import moment from 'moment';
 
 export default (expenses, { text, sortBy, startDate, endDate }) => {
     console.log(expenses);
     return expenses.filter((expense) => {
+        const createdAtMoment = moment(expense.createdAt);
         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
-        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
-        const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+        const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
+        const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
         
         return textMatch && startDateMatch && endDateMatch;
     }).sort((a, b) => {
