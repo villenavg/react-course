355a1733ba845c4f72c7d4a7ecd06ab3b7efc02f
Alberto Villena <alberto.villenaguerras@telefonica.com>
2019-02-11 17:40:58 +0100

    section12-lesson8

--- expensify-app/src/components/Header.js
+++ expensify-app/src/components/Header.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => (
     <header>
-        <h1>Expensify</h1>
+        <h1>Expensify</h1>u
         <NavLink to="/" activeClassName="is-active" exact>Home page</NavLink>
         <NavLink to="/create" activeClassName="is-active">Create expense</NavLink>
         <NavLink to="/help" activeClassName="is-active">Help page</NavLink>
