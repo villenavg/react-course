543d3af016bd2fcd4c41b39d4f2b0d330b431307
Alberto Villena <alberto.villenaguerras@telefonica.com>
2019-02-12 09:24:51 +0100

    section12-lesson9

--- expensify-app/src/tests/components/Header.test.js
+++ expensify-app/src/tests/components/Header.test.js
@@ -1,9 +1,8 @@
-import ReactShallowRenderer from 'react-test-renderer/shallow';
 import React from 'react';
+import { shallow } from 'enzyme';
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header />);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    const wrapper = shallow(<Header />);
+    expect(wrapper).toMatchSnapshot();
 });
