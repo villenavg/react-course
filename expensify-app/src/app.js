13640290649e01d851ea993ccab6171ee1f9c749
Alberto Villena <alberto.villenaguerras@telefonica.com>
2019-02-13 18:57:02 +0100

    section12-lesson11

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -4,10 +4,10 @@ import AppRouter from './routers/AppRouter';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore'
 import { addExpense, removeExpense, editExpense } from './actions/expenses'
-import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore();
 console.log(store.getState());
